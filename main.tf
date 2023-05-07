3a00ac9427a908aacf94dd82e9f1c61d7fa712f5
greyhoundforty <ryantiffany@fastmail.com>
2023-05-07 13:27:04 -0500

    🎉 feat(data.tf, main.tf, variables.tf): add support for VPC creation and security group rules This commit adds support for creating a VPC and security group rules. The `data.tf` file now includes data sources for the module. The `main.tf` file now includes a module for creating a VPC and a module for creating a security group. The `variables.tf` file now includes variables for the VPC and security group. The VPC module creates a VPC with a public gateway and a subnet. The security group module creates a security group with a set of rules. The security group rules are validated to ensure that they are correctly formatted.

--- main.tf
+++ main.tf
@@ -1,9 +1,91 @@
 locals {
   deploy_date = formatdate("YYYYMMDD", timestamp())
+  prefix      = "rtlab-${random_string.prefix.result}-${local.deploy_date}"
+  ssh_key_ids = var.existing_ssh_key != "" ? [data.ibm_is_ssh_key.sshkey[0].id] : [ibm_is_ssh_key.generated_key[0].id]
+  zones       = length(data.ibm_is_zones.regional.zones)
+  vpc_zones = {
+    for zone in range(local.zones) : zone => {
+      zone = "${var.region}-${zone + 1}"
+    }
+  }
+  tags = [
+    "owner:ryantiffany",
+    "project:${local.prefix}",
+    "deploydate:${local.deploy_date}"
+  ]
+}
+
+resource "random_string" "prefix" {
+  length  = 4
+  special = false
+  numeric = false
+  upper   = false
 }
 
 module "resource_group" {
   source                       = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
   resource_group_name          = var.existing_resource_group == null ? "${local.prefix}-resource-group" : null
   existing_resource_group_name = var.existing_resource_group
+}
+
+module "vpc" {
+  source                      = "terraform-ibm-modules/vpc/ibm//modules/vpc"
+  version                     = "1.1.1"
+  create_vpc                  = true
+  vpc_name                    = "${local.project_prefix}-vpc"
+  resource_group_id           = module.resource_group.id
+  classic_access              = false
+  default_address_prefix      = "auto"
+  default_network_acl_name    = "${local.project_prefix}-vpc-default-network-acl"
+  default_security_group_name = "${local.project_prefix}-vpc-default-security-group"
+  default_routing_table_name  = "${local.project_prefix}-vpc-default-routing-table"
+  vpc_tags                    = local.tags
+  locations                   = [local.vpc_zones.0.zone]
+  subnet_name                 = "${local.project_prefix}-frontend-subnet"
+  number_of_addresses         = "128"
+  create_gateway              = true
+  public_gateway_name         = "${local.project_prefix}-vpc-pub-gw"
+  gateway_tags                = local.tags
+}
+
+module "security_group" {
+  source                = "terraform-ibm-modules/vpc/ibm//modules/security-group"
+  version               = "1.1.1"
+  create_security_group = true
+  name                  = "${local.prefix}-frontend-sg"
+  vpc_id                = module.vpc.vpc_id[0]
+  resource_group_id     = module.resource_group.resource_group_id
+  security_group_rules  = local.frontend_rules
+}
+
+resource "ibm_is_instance" "bastion" {
+  name                     = "${local.prefix}-bastion"
+  vpc                      = module.vpc.vpc_id[0]
+  image                    = data.ibm_is_image.base.id
+  profile                  = var.instance_profile
+  resource_group           = module.resource_group.resource_group_id
+  metadata_service_enabled = var.metadata_service_enabled
+
+  boot_volume {
+    name = "${local.prefix}-boot-volume"
+  }
+
+  primary_network_interface {
+    subnet            = module.vpc.subnet_ids[0]
+    allow_ip_spoofing = var.allow_ip_spoofing
+    security_groups   = [module.security_group.security_group_id[0]]
+  }
+  # Need to strip out logging and monitoring key from installer script 
+  # Use installer for consul, nomad, vault  
+  # user_data = templatefile("${path.module}/init.tftpl", { logdna_ingestion_key = module.logging.logdna_ingestion_key, region = local.region, vpc_tag = "vpc:${local.prefix}-vpc" })
+  zone = local.vpc_zones[0].zone
+  keys = local.ssh_key_ids
+  tags = concat(local.tags, ["zone:${local.vpc_zones[0].zone}"])
+}
+
+resource "ibm_is_floating_ip" "bastion" {
+  name           = "${local.prefix}-bastion-public-ip"
+  resource_group = module.resource_group.resource_group_id
+  target         = ibm_is_instance.bastion.primary_network_interface[0].id
+  tags           = concat(local.tags, ["zone:${local.vpc_zones[0].zone}"])
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,191 @@
 variable "existing_resource_group" {
-  description = "Name of an existing Resource Group to use for resources. If not set, a new Resource Group will be created."
+  description = "Resource group to use for all deployed resources. If not specified, a new one will be created."
   type        = string
-  default     = null
+}
+
+variable "project_prefix" {
+  description = "Prefix to be added to all deployed resources. If none provided, one will be automatically generated."
+  type        = string
+}
+
+variable "owner" {
+  description = "Project owner or identifier. This is used as a tag on all supported resources."
+  type        = string
 }
 
 variable "region" {
-  description = "IBM Cloud region where resources will be deployed"
-  default     = "us-south"
+  description = "IBM Cloud Region where resources will be deployed. If not specified, one will be randomly selected. To see available regions, run 'ibmcloud is regions'."
+  type        = string
+}
+
+variable "classic_access" {
+  description = "Allow classic access to the VPC."
+  type        = bool
+  default     = false
+}
+
+variable "default_address_prefix" {
+  description = "The address prefix to use for the VPC. Default is set to auto."
+  type        = string
+  default     = "auto"
+}
+
+variable "existing_ssh_key" {
+  description = "The name of an existing SSH key to use. If not specified, a new key will be created."
+  type        = string
+  default     = ""
+}
+
+variable "instance_profile" {
+  description = "The name of an existing instance profile to use. You can list available instance profiles with the command 'ibmcloud is instance-profiles'."
+  type        = string
+  default     = "cx2-2x4"
+}
+
+variable "image_name" {
+  description = "The name of an existing OS image to use. You can list available images with the command 'ibmcloud is images'."
+  type        = string
+  default     = "ibm-ubuntu-22-04-1-minimal-amd64-3"
+}
+
+variable "allow_ip_spoofing" {
+  description = "Allow IP spoofing on the bastion instance primary interface."
+  type        = bool
+  default     = false
+}
+
+variable "metadata_service_enabled" {
+  description = "Enable the metadata service on the bastion instance."
+  type        = bool
+  default     = true
+}
+
+variable "frontend_rules" {
+  description = "A list of security group rules to be added to the Frontend security group"
+  type = list(
+    object({
+      name      = string
+      direction = string
+      remote    = string
+      tcp = optional(
+        object({
+          port_max = optional(number)
+          port_min = optional(number)
+        })
+      )
+      udp = optional(
+        object({
+          port_max = optional(number)
+          port_min = optional(number)
+        })
+      )
+      icmp = optional(
+        object({
+          type = optional(number)
+          code = optional(number)
+        })
+      )
+    })
+  )
+
+  validation {
+    error_message = "Security group rules can only have one of `icmp`, `udp`, or `tcp`."
+    condition = (var.frontend_rules == null || length(var.frontend_rules) == 0) ? true : length(distinct(
+      # Get flat list of results
+      flatten([
+        # Check through rules
+        for rule in var.frontend_rules :
+        # Return true if there is more than one of `icmp`, `udp`, or `tcp`
+        true if length(
+          [
+            for type in ["tcp", "udp", "icmp"] :
+            true if rule[type] != null
+          ]
+        ) > 1
+      ])
+    )) == 0 # Checks for length. If all fields all correct, array will be empty
+  }
+
+  validation {
+    error_message = "Security group rule direction can only be `inbound` or `outbound`."
+    condition = (var.frontend_rules == null || length(var.frontend_rules) == 0) ? true : length(distinct(
+      flatten([
+        # Check through rules
+        for rule in var.frontend_rules :
+        # Return false if direction is not valid
+        false if !contains(["inbound", "outbound"], rule.direction)
+      ])
+    )) == 0
+  }
+
+  validation {
+    error_message = "Security group rule names must match the regex pattern ^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$."
+    condition = (var.frontend_rules == null || length(var.frontend_rules) == 0) ? true : length(distinct(
+      flatten([
+        # Check through rules
+        for rule in var.frontend_rules :
+        # Return false if direction is not valid
+        false if !can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", rule.name))
+      ])
+    )) == 0
+  }
+
+  default = [
+    {
+      name       = "inbound-vpn-udp"
+      direction  = "inbound"
+      remote     = "0.0.0.0/0"
+      ip_version = "ipv4"
+      udp = {
+        port_min = 51280
+        port_max = 51280
+      }
+    },
+    {
+      name       = "inbound-http"
+      direction  = "inbound"
+      remote     = "0.0.0.0/0"
+      ip_version = "ipv4"
+      tcp = {
+        port_min = 80
+        port_max = 80
+      }
+    },
+    {
+      name       = "inbound-https"
+      direction  = "inbound"
+      remote     = "0.0.0.0/0"
+      ip_version = "ipv4"
+      tcp = {
+        port_min = 443
+        port_max = 443
+      }
+    },
+    {
+      name       = "inbound-ssh"
+      direction  = "inbound"
+      remote     = "0.0.0.0/0"
+      ip_version = "ipv4"
+      tcp = {
+        port_min = 22
+        port_max = 22
+      }
+    },
+    {
+      name       = "inbound-icmp"
+      direction  = "inbound"
+      remote     = "0.0.0.0/0"
+      ip_version = "ipv4"
+      icmp = {
+        code = 0
+        type = 8
+      }
+    },
+    {
+      name       = "all-outbound"
+      direction  = "outbound"
+      remote     = "0.0.0.0/0"
+      ip_version = "ipv4"
+    }
+  ]
 }
